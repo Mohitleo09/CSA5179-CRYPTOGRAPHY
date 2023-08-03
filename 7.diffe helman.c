3f856e0484f9ca4edee9385328cb0162d949a7c0
Mohitleo09 <114983369+Mohitleo09@users.noreply.github.com>
2023-08-03 13:39:54 +0530

    OUTPUT

--- 7.diffe helman.c
+++ 7.diffe helman.c
@@ -1,29 +1,30 @@
-#include <stdio.h>
 #include <math.h>
-int mod_exp(int base, int exp, int modulus) {
-    int result = 1;
-    base = base % modulus;
-    while (exp > 0) {
-        if (exp % 2 == 1) {
-            result = (result * base) % modulus;
-        }
-        exp = exp >> 1;
-        base = (base * base) % modulus;
-    }
-    return result;
+#include <stdio.h>
+long long int power(long long int a, long long int b,
+					long long int P)
+{
+	if (b == 1)
+		return a;
+
+	else
+		return (((long long int)pow(a, b)) % P);
 }
-int main() {
-    int prime = 23; 
-    int base = 5;   
-    int aliceKey = 6; 
-    int bobKey = 15;
-    int alicePublicKey = mod_exp(base, aliceKey, prime);
-    int bobPublicKey = mod_exp(base, bobKey, prime);
-    printf("Alice's Public Key: %d\n", alicePublicKey);
-    printf("Bob's Public Key: %d\n", bobPublicKey);
-    int aliceSharedSecret = mod_exp(bobPublicKey, aliceKey, prime);
-    int bobSharedSecret = mod_exp(alicePublicKey, bobKey, prime);
-    printf("Alice's Shared Secret: %d\n", aliceSharedSecret);
-    printf("Bob's Shared Secret: %d\n", bobSharedSecret);
-    return 0;
+int main()
+{
+	long long int P, G, x, a, y, b, ka, kb;
+	P = 23; 
+	printf("The value of P : %lld\n", P);
+	G = 9; 
+	printf("The value of G : %lld\n\n", G);
+	a = 4; 
+	printf("The private key a for Alice : %lld\n", a);
+	x = power(G, a, P); 
+	b = 3;
+	printf("The private key b for Bob : %lld\n\n", b);
+	y = power(G, b, P); 
+	ka = power(y, a, P); 
+	kb = power(x, b, P); 
+	printf("Secret key for the Alice is : %lld\n", ka);
+	printf("Secret Key for the Bob is : %lld\n", kb);
+	return 0;
 }
